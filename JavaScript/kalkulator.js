d81661cb5d1c698a4f4d1bbe050a593ae3258e86
Marton Bacso <marton.bacso@gmail.com>
2024-01-16 17:26:49 +0100

    Javitasok

--- JavaScript/kalkulator.js
+++ JavaScript/kalkulator.js
@@ -1,3 +1,30 @@
-function fontKilogramKonverter(valNUM) {
-    document.getElementById("outputKilogramm").innerHTML = valNUM * 0.4536;
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+
+function fontKilogrammKonverter(valNUM) {
+document.getElementById("outputKilogramm").innerHTML = round((valNUM / 2.204623), 3);
+}
+
+
+function kilogrammFontKonverter(valNUM) {
+document.getElementById("outputfont").innerHTML = round((valNUM * 2.204623), 3);
+}
+
+function inchCmKonverter(valNUM) {
+document.getElementById("outputCm").innerHTML = round((valNUM * 2.54), 3);
+}
+
+function cmInchKonverter(valNUM) {
+document.getElementById("outputInch").innerHTML = round((valNUM / 2.54), 3);
+}
+
+function feetMeterKonverter(valNUM) {
+document.getElementById("outputMeter").innerHTML = round((valNUM / 3.28084), 3);
+}
+
+function meterFeetKonverter(valNUM) {
+document.getElementById("outputFeet").innerHTML = round((valNUM * 3.28084), 3);
 }
---
+++ Kalkulator/JavaScript/kalkulator.js
@@ -0,0 +1,14 @@
+function fontKilogramKonverter(valNUM) {
+    document.getElementById("outputKilogramm").innerHTML = valNUM * 0.4536;
+}
+
+function kilogramFontKonverter(valNUM) {
+    document.getElementById("outputFont").innerHTML = valNUM * 2.2;
+}
+function centimeterInchKonverter(valNUM) {
+    document.getElementById("outputInch").innerHTML = valNUM * 0.4;
+} 
+
+functionInchCentimeterKonventer(valNUM) {
+    document.getElementById("outputCentimeter").innerHTML = valNUM * 2.5;
+}
